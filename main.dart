943121608df14274db7801f0d859506eb9f51614
zymfe <1047832475@qq.com>
2020-03-06 12:42:47 +0800

    update

--- main.dart
+++ main.dart
@@ -5,5 +5,14 @@ void main () {
   var b;
   print(a); // 123
   print(b); // null
-}
+  a = 456;
+  print(a);
 
+  // final 和 const 是不一样的，后面面向对象详细学习
+  
+  final c = 'hello';
+  print(c);
+  
+  const d = 'world';
+  print(d);
+}
